ff5c4f671a0b4aafbce2e467a8d99cecbb019385
Ksolves <chandresh.joshi@gmail.com>
2020-07-07 19:59:20 +0530

    visualforce learning

---
+++ force-app/main/default/staticresources/demojs.js
@@ -0,0 +1,8 @@
+function changefont(input,textid){
+if(input.checked){
+    document.getElementById.style.fontWeight="bold";
+}
+else {
+    document.getElementById(textid).style.fontWeight="normal";
+}
+}
